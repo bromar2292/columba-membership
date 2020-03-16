7f739aff8131fee8363afa0374a1404887358961
omar mahmood sultani <bromar2292@googlemail.com>
2020-03-16 13:41:13 +0000

    firebase added github added font added padding sorted

--- src/index.js
+++ src/index.js
@@ -3,20 +3,21 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "../src/app/App";
 import "./scss/_.scss";
-import "./font/Mont.otf";
-import "./font/EULA Free Font License Ver. 2.0.pdf";
-import "./font/Mont-HeavyDEMO.otf";
-import "./font/Mont_Specimen.pdf";
+
+import "./fonts/Mont-Heavy.otf";
+import "./fonts/Mont-Light.otf";
+
 import * as serviceWorker from "./serviceWorker";
 
-import {
-  BrowserRouter as Router
-}
+import { BrowserRouter as Router } from "react-router-dom";
 
-from "react-router-dom";
-
-ReactDOM.render(<Router> <App /> </Router>,
-  document.getElementById("root"));
+ReactDOM.render(
+  <Router>
+    {" "}
+    <App />{" "}
+  </Router>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
